61d1a144d61ff2315c65e7ed3c2babadf015a286
000nikita0007 <111862222+000nikita0007@users.noreply.github.com>
2022-11-10 15:35:34 +0300

    My homeworkN5_3

--- src/Lesson_5_1/N3.java
+++ src/Lesson_5_1/N3.java
@@ -1,6 +1,17 @@
 package Lesson_5_1;
+
+import java.util.Scanner;
+
 public class N3 {
     public static void main(String[] args) {
+        System.out.println("Enter size of array");
+        int n = new Scanner(System.in).nextInt();
+        int m = new Scanner(System.in).nextInt();
+        int array[][] = new int[n][m];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
 
+            }
+        }
     }
 }
