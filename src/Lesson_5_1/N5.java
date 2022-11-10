b56488f3a74f150938dc4345d61c6196eebbf984
000nikita0007 <111862222+000nikita0007@users.noreply.github.com>
2022-11-10 18:39:31 +0300

    My homeworkN4

--- src/Lesson_5_1/N5.java
+++ src/Lesson_5_1/N5.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public class N5 {
     public static void main(String[] args) {
+        boolean isSorted = false;
+        int buf;
         System.out.println("Enter massive size n x m");
         int n = new Scanner(System.in).nextInt();
         int m = new Scanner(System.in).nextInt();
         int array[][] = new int[n][m];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = ((int) (Math.random() * 100));
+                array[i][j] = ((int) (Math.random() * 10));
             }
         }
         System.out.println(Arrays.deepToString(array).replace("], ", "]\n"));
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; i++) {
-                Arrays.sort(array);
-            }
-        }
+        for(int i=0;i<array.length;i++)
+        {Arrays.sort(array[i]);}
+        System.out.println(Arrays.deepToString(array).replace("], ", "]\n"));
     }
 }
