b54278d388ea7c6dcb5729f513ac29c7ca9d7805
000nikita0007 <111862222+000nikita0007@users.noreply.github.com>
2022-11-17 00:10:02 +0300

    N6_1 homework

--- src/Lesson6/CreditCard.java
+++ src/Lesson6/CreditCard.java
@@ -3,9 +3,8 @@ package Lesson6;
 import java.util.Scanner;
 
 public class CreditCard {
-    static String account;
-    static int currentSum;
-
+   String account;
+  int currentSum;
     public CreditCard(String account, int currentSum) {
         this.account = account;
         this.currentSum = currentSum;
@@ -19,19 +18,21 @@ public class CreditCard {
                 '}';
     }
 
-    public static void Accrual(){
-        System.out.println("Введите сумму начисления");
-        int sum=new Scanner(System.in).nextInt();
-        currentSum+=sum;
+    public  void Accrual() {
+        System.out.println("Введите сумму начисления на номер счёта  " + account);
+        int sum = new Scanner(System.in).nextInt();
+        currentSum += sum;
 
     }
-    public static void Remove(){
-        System.out.println("Введите сумму списания");
-         int sum=new Scanner(System.in).nextInt();
-         currentSum-=sum;
+
+    public  void Remove() {
+        System.out.println("Введите сумму списания по номеру" + account);
+        int sum = new Scanner(System.in).nextInt();
+        currentSum -= sum;
     }
-    public static void Condition(){
-        System.out.println("Текущее состояние карточки по номеру счёта "+ account +" Текущая сумма на счету= "+ currentSum);
+
+    public void toString1() {
+        System.out.println("Текущее состояние карточки по номеру счёта " + account + " Текущая сумма на счету= " + currentSum);
     }
 
 }
--- src/Lesson6/Main.java
+++ src/Lesson6/Main.java
@@ -2,11 +2,15 @@ package Lesson6;
 
 public class Main {
     public static void main(String[] args) {
-        CreditCard creditCard1 = new CreditCard("by2321",4321);
-        System.out.println(creditCard1.toString());
+        CreditCard creditCard1 = new CreditCard("by23241",4321);
+        CreditCard creditCard2 = new CreditCard("by6321321241",3213);
+        CreditCard creditCard3 = new CreditCard("by654311",6543);
+        creditCard1.toString1();
+        creditCard2.toString1();
+        creditCard3.toString1();
         creditCard1.Accrual();
-        System.out.println((creditCard1.toString()));
-        creditCard1.Remove();
-        creditCard1.Condition();
+        creditCard2.Accrual();
+        creditCard3.Remove();
+        System.out.println(creditCard1.toString()+"\n"+creditCard2.toString()+"\n"+creditCard3.toString());
     }
 }
