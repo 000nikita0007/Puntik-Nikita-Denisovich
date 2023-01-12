65af4fadea509e8356336fbe7d620991e934f875
000nikita0007 <111862222+000nikita0007@users.noreply.github.com>
2023-01-12 13:47:55 +0300

    N19_1_1

--- src/Lesson_19/MyRunnable.java
+++ src/Lesson_19/MyRunnable.java
@@ -5,5 +5,11 @@ public class MyRunnable implements Runnable  {
     @Override
     public void run() {
         System.out.println("Запущен поток " +Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Поток отработал:::" + Thread.currentThread().getName());
     }
 }
