4ca85d0b19b3a4b133208a0cdbfc51fdb7baf551
000nikita0007 <111862222+000nikita0007@users.noreply.github.com>
2023-01-12 13:53:52 +0300

    N19_1_1

--- src/Lesson_19/Main.java
+++ src/Lesson_19/Main.java
@@ -6,14 +6,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         MyRunnable myRunnable1 = new MyRunnable();
-        Thread thread1 = new Thread(myRunnable1,"T1");
-        Thread thread2 = new Thread(myRunnable1,"T2");
-        Thread thread3 = new Thread(myRunnable1,"T3");
+        Thread thread1 = new Thread(myRunnable1, "T1");
+        Thread thread2 = new Thread(myRunnable1, "T2");
+        Thread thread3 = new Thread(myRunnable1, "T3");
         thread3.start();
-        thread3.join(2000);
+        try {
+            thread3.join(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         thread2.start();
-        thread2.join(3000);
+        try {
+            thread2.join(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         thread1.start();
-        thread1.join(4000);
+        try {
+            thread1.join(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Все потоки отработали");
     }
 }
--- src/Lesson_19/MyRunnable.java
+++ src/Lesson_19/MyRunnable.java
@@ -1,10 +1,10 @@
 package Lesson_19;
 
-public class MyRunnable implements Runnable  {
+public class MyRunnable implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Запущен поток " +Thread.currentThread().getName());
+        System.out.println("Запущен поток " + Thread.currentThread().getName());
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
